25df6a7185d38236f47627b29e5e7d41b4cde43b
Will Hay Jr <willhayjr@gmail.com>
2020-06-10 14:37:15 -0400

    Update cracka.py

--- cracka.py
+++ cracka.py
@@ -7,13 +7,13 @@ client = datastore.Client()
 key = client.key('seedWords', 5634161670881280)
 entity = client.get(key)
 possible_seeds = entity['possible_seeds']
-
+print(possible_seeds)
 your8words = "police online cradle arena spice sport draw invite"
 
 m = mnemonic.Mnemonic('english')
 
 perms = list(itertools.permutations(possible_seeds))
-
+print(len(perms))
 for words in perms:
     key = your8words
     for word in words:
