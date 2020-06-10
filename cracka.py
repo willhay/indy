f8f27cdc1a30fd9ef32cb7765b54d84f65698f8c
Will Hay Jr <willhayjr@gmail.com>
2020-06-10 18:11:17 -0400

    first prod change

--- cracka.py
+++ cracka.py
@@ -6,26 +6,46 @@ import asyncio
 
 
 def takeCoins():
-    client = datastore.Client()
-    key = client.key('seedWords', 5634161670881280)
-    entity = client.get(key)
-    possible_seeds = entity['possible']
-    print(possible_seeds)
+    # client = datastore.Client()
+    # key = client.key('seedWords', 5634161670881280)
+    # entity = client.get(key)
+    # possible_seeds = entity['possible']
+    # print(possible_seeds)
 
-    # possible_seeds = ["" ,"", "", ""]
-    your8words = "jelly motor pull buzz tongue dawn trim estate"
+    # possible_seeds = ["attract" ,"what", "friend", "abandon"]
+    # your8words1 = "army excuse hero wolf disease rebuild ability"
+    # your8words2 = "army excuse hero wolf disease liberty ability"
 
+    possible_seeds = ["grit", "rug", "girl", "baby"]
     m = mnemonic.Mnemonic('english')
 
     perms = list(itertools.permutations(possible_seeds, 4))
     valid_keys = []
     print(len(perms))
+    your8words = "motor worry mean random pulse scan theme coffee"
+
     for words in perms:
         key = your8words
         for word in words:
             key += ' ' + word
         if m.check(key):
             valid_keys.append(key)
-
-    # TODO autobroadcast!!
+    print(valid_keys)
     main(valid_keys, '1992vwyicuszhwrSJKxGJxmtDAqWTe7pS3', True)
+
+    your8words = "motor worry mean random pulse disease theme coffee"
+
+    valid_keys = []
+    print(len(perms))
+    for words in perms:
+        key = your8words
+        for word in words:
+            key += ' ' + word
+            key2 += ' ' + word
+        if m.check(key):
+            valid_keys.append(key)
+    print(valid_keys)
+    main(valid_keys, '1992vwyicuszhwrSJKxGJxmtDAqWTe7pS3', True)
+
+
+takeCoins()
