0db472c9b5ce331d0fd21bf99254551e9b602f6b
Will Hay Jr <willhayjr@gmail.com>
2020-06-10 14:49:58 -0400

    Update cracka.py

--- cracka.py
+++ cracka.py
@@ -4,27 +4,29 @@ from indy import main
 from google.cloud import datastore
 
 
-async def main():
-    client = datastore.Client()
-    key = client.key('seedWords', 5634161670881280)
-    entity = client.get(key)
-    possible_seeds = entity['possible_seeds']
-    print(possible_seeds)
-    your8words = "police online cradle arena spice sport draw invite"
+client = datastore.Client()
+key = client.key('seedWords', 5634161670881280)
+entity = client.get(key)
+possible_seeds = entity['possible_seeds']
+print(possible_seeds)
+your8words = "police online cradle arena spice sport draw invite"
 
-    m = mnemonic.Mnemonic('english')
+m = mnemonic.Mnemonic('english')
 
-    perms = list(itertools.permutations(possible_seeds, 4))
-    valid_keys = []
-    print(len(perms))
-    for words in perms:
-        key = your8words
-        for word in words:
-            key += ' ' + word
-        if m.check(key):
-            print(key)
-            valid_keys.append(key)
+perms = list(itertools.permutations(possible_seeds, 4))
+valid_keys = []
+print(len(perms))
+for words in perms:
+    key = your8words
+    for word in words:
+        key += ' ' + word
+    if m.check(key):
+        valid_keys.append(key)
 
-    print(valid_keys)
-    for key in valid_keys:
-        await main(valid_keys[0], '1992vwyicuszhwrSJKxGJxmtDAqWTe7pS3', False)
+test_seeds(valid_keys)
+
+
+async def test_seeds(keys):
+    print(keys)
+    for key in keys:
+        await main(keys[0], '1992vwyicuszhwrSJKxGJxmtDAqWTe7pS3', False)
