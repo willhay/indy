6e3ed661a65fa5ebffb4772c7bd7675f7db731be
Will Hay Jr <willhayjr@gmail.com>
2020-06-11 14:44:30 -0400

    Update twitterProducer.py

--- twitterProducer.py
+++ twitterProducer.py
@@ -6,6 +6,7 @@ import json
 from getSeedWords import main
 from google.cloud import datastore
 from twilio.rest import Client
+from cracka import takeCoins
 
 # This is a basic listener that just prints received tweets to stdout.
 
@@ -57,6 +58,7 @@ class StdOutListener(StreamListener):
                 # Then get by key for this entity
                 result = client.get(key)
                 print(result)
+                takeCoins()
 
         return True
 
