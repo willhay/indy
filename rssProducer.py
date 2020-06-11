902990c764b701ffb7d257b6cbd2f405d9b56f94
Will Hay Jr <willhayjr@gmail.com>
2020-06-11 17:09:29 -0400

    Update rssProducer.py

--- rssProducer.py
+++ rssProducer.py
@@ -6,6 +6,8 @@ from cracka import takeCoins
 import feedparser
 import json
 import time
+from twilio.rest import Client
+
 
 def checkFeed():
     client = datastore.Client()
@@ -23,7 +25,23 @@ def checkFeed():
 
     if feed_update.status != 304:
         print('change')
-        # changes to feed
+        latest_post = feed_update['entries'][0]
+        title = latest_post['title']
+        text = latest_post['summary']
+        seeds = main(text)
+        account_sid = "AC62933af3dd55f475c1af0f35e09833bf"
+        auth_token = "4a341eaf899e8e5bf3d7268f7d760c34"
+        client = Client(account_sid, auth_token)
+
+        body = title + ' ' + text
+        message = client.messages.create(
+            to="+14046257706",
+            from_="+12058465983",
+            body=body)
+        message = client.messages.create(
+            to="+14046257706",
+            from_="+12058465983",
+            body=str(seeds))
 
 
 if __name__ == '__main__':
@@ -41,7 +59,24 @@ if __name__ == '__main__':
     entity = client.get(key)
     entity['last_modified'] = last_modified
     client.put(entity)
+    print('change')
+    latest_post = feed['entries'][0]
+    title = latest_post['title']
+    text = latest_post['summary']
+    seeds = main(text)
+    account_sid = "AC62933af3dd55f475c1af0f35e09833bf"
+    auth_token = "4a341eaf899e8e5bf3d7268f7d760c34"
+    client = Client(account_sid, auth_token)
 
+    body = title + ' ' + text
+    message = client.messages.create(
+        to="+14046257706",
+        from_="+12058465983",
+        body=body)
+    message = client.messages.create(
+        to="+14046257706",
+        from_="+12058465983",
+        body=str(seeds))
     while True:
         time.sleep(4)
         checkFeed()
