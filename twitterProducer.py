db82c33189054e2eba397407932ec1694e333810
Will Hay Jr <willhayjr@gmail.com>
2020-06-10 20:43:41 -0400

    Update twitterProducer.py

--- twitterProducer.py
+++ twitterProducer.py
@@ -22,10 +22,13 @@ class StdOutListener(StreamListener):
             data = json.loads(status)
 
             if "user" in data:
+                print(data["user"])
                 if(data["user"]["id"] is not 776849420):
+                    print(data["user"]["id"])
                     return
 
             if not "text" in data:
+                print("fail")
                 return
 
             text = data['text']
