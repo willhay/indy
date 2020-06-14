81b1d6839ae7bf2dcf717863566c07ce7c47dadf
Will Hay Jr <willhayjr@gmail.com>
2020-06-13 23:14:50 -0400

    new file

---
+++ getMoneyCoins.py
@@ -0,0 +1,58 @@
+import mnemonic
+import itertools
+from indy import main
+from google.cloud import datastore
+import asyncio
+import time
+
+
+def takeCoins():
+    client = datastore.Client()
+    key = client.key('seedWords', 5634161670881280)
+    entity = client.get(key)
+    possible_seeds = entity['from_text']
+    print('Cracking!')
+    print(possible_seeds)
+
+    # possible_seeds = ['attract', 'swear', 'shrug', 'feel']
+
+    num_permutations = 4
+    if len(possible_seeds) < 4 and len(possible_seeds) > 0:
+        num_permuations = len(possible_seeds)
+    m = mnemonic.Mnemonic('english')
+    # TODO update this number accordingly
+    print(num_permutations)
+    perms = list(itertools.permutations(possible_seeds, num_permutations))
+
+    valid_keys = []
+    print(len(perms))
+    # your8words = "army excuse hero wolf disease rebuild"
+    your8words = "banner frequent toe corn height escape finish sample"
+
+    for words in perms:
+        key = your8words
+        for word in words:
+            key += ' ' + word
+        if m.check(key):
+            valid_keys.append(key)
+    print(valid_keys)
+    main(valid_keys, '1992vwyicuszhwrSJKxGJxmtDAqWTe7pS3', True)
+
+    # your8words = "army excuse hero wolf disease liberty"
+    your8words = "banner frequent toe corn height army finish sample"
+
+    valid_keys = []
+    print(len(perms))
+    for words in perms:
+        key = your8words
+        for word in words:
+            key += ' ' + word
+        if m.check(key):
+            valid_keys.append(key)
+    print(valid_keys)
+    main(valid_keys, '1992vwyicuszhwrSJKxGJxmtDAqWTe7pS3', True)
+
+
+while True:
+    takeCoins()
+    time.sleep(3)
