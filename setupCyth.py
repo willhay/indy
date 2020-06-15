9a1e7d0ff9b224b56c30f57c2f724206107f2676
Will Hay Jr <willhayjr@gmail.com>
2020-06-15 17:32:28 -0400

    Update setupCyth.py

--- setupCyth.py
+++ setupCyth.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
     name='Hello world app',
     ext_modules=cythonize("weball.pyx"),
     zip_safe=False,
+    include_dirs=[numpy.get_include()],
 )
