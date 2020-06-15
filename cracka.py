0a27ebc04093f2da720dca87c5df7073cd239714
Will Hay Jr <willhayjr@gmail.com>
2020-06-15 14:31:13 -0400

    manual tools and fix for cracka.py

--- cracka.py
+++ cracka.py
@@ -16,14 +16,13 @@ def takeCoins():
 
     num_permutations = 4
     if len(possible_seeds) < 4 and len(possible_seeds) > 0:
-        num_permuations = len(possible_seeds)
+        num_permutations = len(possible_seeds)
     m = mnemonic.Mnemonic('english')
     # TODO update this number accordingly
     perms = list(itertools.permutations(possible_seeds, num_permutations))
 
     valid_keys = []
-    print(len(perms))
-    your8words = "army excuse hero wolf disease rebuild moral"
+    your8words = "army excuse hero wolf disease liberty moral diagram"
 
     for words in perms:
         key = your8words
@@ -31,13 +30,12 @@ def takeCoins():
             key += ' ' + word
         if m.check(key):
             valid_keys.append(key)
-    print(valid_keys)
+    print('valid_phrases: ', valid_keys)
     main(valid_keys, '1992vwyicuszhwrSJKxGJxmtDAqWTe7pS3', True)
 
-    your8words = "army excuse hero wolf disease liberty moral"
+    your8words = "army excuse hero wolf disease rebuild moral diagram"
 
     valid_keys = []
-    print(len(perms))
     for words in perms:
         key = your8words
         for word in words:
---
+++ manualTake.py
@@ -0,0 +1,53 @@
+import mnemonic
+import itertools
+from indy import main
+from google.cloud import datastore
+import asyncio
+import time
+
+
+def takeCoins(last_seeds):
+
+    # last_seeds = ["attract" ,"what", "friend", "abandon"]
+
+    num_permutations = 4
+
+    num_words = len(last_seeds.split())
+
+    if (num_words) < 4 and (num_words) > 0:
+        num_permutations = (num_words)
+    m = mnemonic.Mnemonic('english')
+    perms = list(itertools.permutations(last_seeds.split(), num_permutations))
+    valid_keys = []
+    your8words = "army excuse hero wolf disease liberty moral diagram"
+
+    for words in perms:
+        key = your8words
+        for word in words:
+            key += ' ' + word
+        if m.check(key):
+            valid_keys.append(key)
+    print('valid_phrases: ', valid_keys)
+    main(valid_keys, '1992vwyicuszhwrSJKxGJxmtDAqWTe7pS3', True)
+
+    your8words = "army excuse hero wolf disease rebuild moral diagram"
+
+    valid_keys = []
+    for words in perms:
+        key = your8words
+        for word in words:
+            key += ' ' + word
+        if m.check(key):
+            valid_keys.append(key)
+    print('valid_phrases: ', valid_keys)
+    main(valid_keys, '1992vwyicuszhwrSJKxGJxmtDAqWTe7pS3', True)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Create a ArcHydro schema')
+    parser.add_argument('--seeds',
+                        help='path to dem')
+    args = parser.parse_args()
+    takeCoins(last_seeds=args.seeds)
